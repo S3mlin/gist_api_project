379eda8c86f32829dae5e790275415e2ccc102e2
S3mlin <lsemic97@gmail.com>
2022-07-25 16:38:03 +0200

    celery testing

--- app/routes.py
+++ app/routes.py
@@ -12,29 +12,12 @@ import datetime
 bp = Blueprint("bp", __name__)
 
 
-@celery.task
-def send_async_text(test):
-    with current_app.app_context():
-        return test
-
 @celery.task(bind=True)
-def long_task(self):
-    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
-    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
-    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
-    message = ''
-    total = random.randint(10, 50)
-    for i in range(total):
-        if not message or random.random() < 0.25:
-            message = '{0} {1} {2}...'.format(random.choice(verb),
-                                            random.choice(adjective),
-                                            random.choice(noun))
-        self.update_state(state='PROGRESS',
-                        meta={'current': i, 'total': total,
-                                'status': message})
-        time.sleep(1)
-    return {'current': 100, 'total': 100, 'status': 'Task Completed!',
-            'result': 42}
+def button_test(self, test):
+    self.update_state(state='PENDING')
+    time.sleep(5)
+    self.update_state(state='COMPLETE')
+    return test
 
 
 @bp.route('/ping')
@@ -104,12 +87,12 @@ def search():
     test = request.form['test']
     session['test'] = test
     if request.form['submit'] == 'Send':
-        send_async_text.delay(test)
+        button_test.delay(test)
         flash('Sending text to {0}'.format(test))
     else:
-        send_async_text.apply_async(args=[text_sample], countdown=60)
+        send_async_text.apply_async(args=[test], countdown=60)
         flash('Text will be sent to {0} in one minute'.format(test))
-    return render_template('index.html', title='Home',form=form, test=session.get('test'))
+    return render_template('index.html', title='Home',form=form)
 
 
 @bp.route('/api/v1/search/results', methods=['GET', 'POST'])
@@ -130,34 +113,13 @@ def results():
 
 @bp.route('/longtask', methods=['POST'])
 def longtask():
-    task = long_task.apply_async()
+    task = button_test.apply_async()
     return jsonify({'Location': url_for('bp.taskstatus', task_id=task.id)}), 202
-
 
 @bp.route('/status/<task_id>')
 def taskstatus(task_id):
-    task = long_task.AsyncResult(task_id)
-    if task.state == 'PENDING':
-        end = {
-            'state': task.state,
-            'current': 0,
-            'total': 1,
-            'status': 'Pending...'
-        }
-    elif task.state != 'FAILURE':
-        end = {
-            'state': task.state,
-            'current': task.info.get('current', 0),
-            'total': task.info.get('total', 1),
-            'status': task.info.get('status', '')
-        }
-        if 'result' in task.info:
-            end['result'] = task.info['result']
-    else:
-        end = {
-            'state': task.state,
-            'current': 1,
-            'total': 1,
-            'status': str(task.info)
-        }
-    return jsonify(end)
+    task = button_test.AsyncResult(task_id)
+    response = {
+        'state': task.state
+    }
+    return jsonify(response)
